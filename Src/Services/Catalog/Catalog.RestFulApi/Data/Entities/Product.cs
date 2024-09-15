3a1b1202400449de7edfb5708136474bf7d00b9d
baabak aghaei <baabakaghaei@gmail.com>
2024-09-16 00:22:47 +0330

    adding mondodb and enitiy

---
+++ Src/Services/Catalog/Catalog.RestFulApi/Data/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.RestFulApi.Data.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement(nameof(Name))]
+        public string Name { get; set; }
+        [BsonElement(nameof(Catagory))]
+        public string Catagory { get; set; }
+        [BsonElement(nameof(Summery))]
+        public string Summery { get; set; }
+        [BsonElement(nameof(Price))]
+        public decimal Price { get; set; }
+    }
+}
