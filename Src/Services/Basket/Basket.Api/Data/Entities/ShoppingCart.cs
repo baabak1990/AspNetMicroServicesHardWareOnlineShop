e55c042b4f253a9a87e110f308c2bd0ed17fee08
baabak aghaei <baabakaghaei@gmail.com>
2024-09-20 16:17:28 +0330

    Adding Redis to Project and Docker

---
+++ Src/Services/Basket/Basket.Api/Data/Entities/ShoppingCart.cs
@@ -0,0 +1,34 @@
+﻿namespace Basket.Api.Data.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string userName)
+        {
+            this.UserName = userName;
+        }
+
+        public decimal TaotalPrice
+        {
+            get
+            {
+                //Calculate the total price of specific shopping cart
+                decimal totalPrice = 0;
+                foreach (var item in Items)
+                {
+                    totalPrice += item.Price * item.Quantity;
+                }
+                return totalPrice;
+            }
+
+        }
+    }
+
+}
