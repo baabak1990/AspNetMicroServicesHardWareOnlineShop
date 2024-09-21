54f69cf9f9111f53b11c5cf7f10e33458df52eeb
baabak aghaei <baabakaghaei@gmail.com>
2024-09-21 16:29:34 +0330

    discount repository

---
+++ Src/Services/Discount/Data/Repository/IDiscountRepository.cs
@@ -0,0 +1,11 @@
+﻿using Discount.Data.Entity;
+
+namespace Discount.Data.Repository;
+
+public interface IDiscountRepository
+{
+    Task<Coupon> GetDiscount(string productName);
+    Task<bool> CreateDiscount(Coupon coupone);
+    Task<bool> UpdateDiscount(Coupon coupon);
+    Task<bool> DeleteDiscount(string productName);
+}
